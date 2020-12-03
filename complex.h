77ac5e85c3f10252586e0be60387a98b6bfd8863
Yatin Khanna <yatinkhanna2000@gmail.com>
2020-12-04 03:15:26 +0530

    Added support for I/O operators and real operands

--- complex.h
+++ complex.h
@@ -6,10 +6,10 @@ The following functions are supported:
     2. imag() : returns imaginary part of the complex number
     3. mod() : returns modulus of the complex number
     4. arg() : returns argument of the complex number (in radians)
-    5. argInDegrees : returns argumnt of the complex number (in degrees)
+    5. argInDegrees() : returns argumnt of the complex number (in degrees)
 
-The following operators are supported:
-    +, -, *, /, +=, -=, *=, /=
+The following operators are supported (both with other complex numbers and real constants):
+    +, -, *, /, +=, -=, *=, /=, <<, >> (output/input)
 
     Two unary operators are also supported:
     ! (returns conjugate of the complex number)
@@ -21,94 +21,57 @@ The following operators are supported:
 
 #include<iostream>
 #include<cmath>
+#include<string>
 
 class Complex{
     double realPart, imaginaryPart;
     public:
 
-    Complex(){
-        realPart = imaginaryPart = 0.0;
-    }
-    Complex(double real, double imaginary){
-        realPart = real;
-        imaginaryPart = imaginary;
-    }
+    Complex();
+    Complex(double real);
+    Complex(double real, double imaginary);
 
-    Complex operator + (Complex &other){
-        return Complex(realPart + other.realPart, imaginaryPart + other.imaginaryPart);
-    }
-    void operator += (Complex &other){
-        *this = *this + other;
-    }
+    void setReal(double real);
+    void setImag(double imag);
 
-    Complex operator - (Complex &other){
-        return Complex(realPart - other.realPart, imaginaryPart - other.imaginaryPart);
-    }
-    void operator -= (Complex &other){
-        *this = *this - other;
-    }
+    Complex operator + (Complex other);
+    void operator += (Complex other);
+    Complex operator + (double other);
+    void operator += (double other);
+    friend Complex operator + (double a, Complex C);
 
-    Complex operator * (Complex &other){
-        return Complex(realPart*other.realPart - imaginaryPart*other.imaginaryPart,
-                       realPart*other.imaginaryPart + imaginaryPart*other.realPart);
-    }
-    void operator *= (Complex &other){
-        *this = *this * other;
-    }
+    Complex operator - (Complex other);
+    void operator -= (Complex other);
+    Complex operator - (double other);
+    void operator -= (double other);
+    friend Complex operator - (double a, Complex C);
 
-    Complex operator / (Complex &other){
-        Complex conjugate = !other;
-        conjugate *= *this;
-        double modSquare = other.mod() * other.mod();
-        conjugate.realPart /= modSquare;
-        conjugate.imaginaryPart /= modSquare;
-        return conjugate;
-    }
-    void operator /= (Complex &other){
-        *this = *this / other;
-    }
+    Complex operator * (Complex other);
+    void operator *= (Complex other);
+    Complex operator * (double other);
+    void operator *= (double other);
+    friend Complex operator * (double a, Complex C);
 
-    Complex operator -(){
-        return Complex(-realPart, -imaginaryPart);
-    }
-    Complex operator !(){
-        return Complex(realPart, -imaginaryPart);
-    }
+    Complex operator / (Complex other);
+    void operator /= (Complex other);
+    Complex operator / (double other);
+    void operator /= (double other);
+    friend Complex operator / (double a, Complex C);
 
-    double real(){
-        return realPart;
-    }
-    double imag(){
-        return imaginaryPart;
-    }
+    Complex operator -();
+    Complex operator !();
 
-    double mod(){
-        return sqrt(realPart*realPart + imaginaryPart*imaginaryPart);
-    }
+    double real();
+    double imag();
 
-    double arg(){
-        if(realPart == 0.0){
-            if(imaginaryPart == 0.0)
-                return 0.0;
-            else
-                return imaginaryPart/fabs(imaginaryPart) * M_PI/2.0;
-        }
-        if(imaginaryPart == 0.0)
-            return 0 + (realPart<0 ? M_PI : 0);
-        double adjust = 0.0;
-        if(realPart < 0){
-            if(imaginaryPart < 0){
-                adjust = -M_PI;
-            }
-            else{
-                adjust = M_PI;
-            }
-        }
-        return atan(imaginaryPart/realPart) + adjust;
-    }
-    double argInDegrees(){
-        return (this->arg())*(180.0/M_PI);
-    }
+    double mod();
+    double arg();
+    double argInDegrees();
+
+    friend std::ostream& operator << (std::ostream &output, Complex C);
+
+    static double stringToDouble(std::string toConvert);
+    friend std::istream& operator >> (std::istream &input, Complex C);
 };
 
 #endif
