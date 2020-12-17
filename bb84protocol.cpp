84cb6973cc86b1c5921a1cd85a3c2f71d3d5481b
chirag-w <chirag_w@cs.iitr.ac.in>
2020-12-17 21:05:15 +0530

    Bug fixes

--- bb84protocol.cpp
+++ bb84protocol.cpp
@@ -1,59 +1,45 @@
-#include "qubits.h"
 #include "circuit.h"
-#include "gate.h"
 
 #include <vector>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include<time.h>
 
-
-std::vector<Circuit*> encode_message(std::vector<int> bits, std::vector<int> bases) {
-	std::vector<Circuit*> message;
-	for(int i=0;i<bits.size();i++) {
-		Circuit *qc = new Circuit(1);
-		if(bases[i] == 0) {
-		// Prepare qubit in Z-basis
-            if (bits[i] == 0){
-                continue;
-			}
-            else {
-                qc->apply(X(), 0);
-        	}
+Circuit encode_message(int bit, int basis) {
+	Circuit qc(1);
+	if(basis == 0) {
+	// Prepare qubit in Z-basis
+        if (bit == 0);
+        else {
+            qc.apply(X(), 0);
+    	}
+	}
+    else {
+	// Prepare qubit in X-basis
+        if (bit == 0){
+            qc.apply(H(), 0);
     	}
         else {
-		// Prepare qubit in X-basis
-            if (bits[i] == 0){
-                qc->apply(H(), 0);
-        	}
-            else {
-                qc->apply(X(), 0);
-                qc->apply(H(), 0);
-        	}
+            qc.apply(X(), 0);
+            qc.apply(H(), 0);
     	}
-        //qc.barrier()
-        message.push_back(qc);
 	}
-	return message;
+	return qc;
 }
 
-std::vector<int> measure_message(std::vector<Circuit*> message, std::vector<int> bases) {
+int measure_message(Circuit& message,int basis) {
 
-    
-    std::vector<int> measurements;
-    for(int q=0;q<bases.size();q++) {
-        if(bases[q] == 0){
+    int measurements;
+    if(basis == 0){
 		// measuring in Z-basis
-            message[q]->measure(0);
-    	}
-        if (bases[q] == 1 ){
-        	// measuring in X-basis
-            message[q]->apply(H(), 0);
-            message[q]->measure(0);
-    	}
-        int measured_bit = message[q]->measureAll()[0];
-        measurements.push_back(measured_bit);
-	}
+        measurements = message.measure(0);
+    }
+    else{
+    	// measuring in X-basis
+        message.apply(H(), 0);
+        measurements = message.measure(0);
+    }
     return measurements;
 }
 
@@ -68,7 +54,7 @@ std::vector<int> remove_garbage(std::vector<int> a_bases, std::vector<int> b_bas
     return good_bits;
 }
 
-std::vector<int> sample_bits(std::vector<int> bits, std::vector<int> selection) {
+std::vector<int> sample_bits(std::vector<int> &bits, std::vector<int> selection) {
     std::vector<int> sample;
     for(int i=0;i<selection.size();i++){
         int index = selection[i]%bits.size();
@@ -78,11 +64,9 @@ std::vector<int> sample_bits(std::vector<int> bits, std::vector<int> selection) 
     return sample;
 }
 
-void apply_protocol() {
-	
-	int n = 100;
-
+void apply_BB84(int n) {
 	// Alice generates bits
+	srand(time(0));
 	std::vector<int> alice_bits;
 	for(int i=0;i<n;i++){
 		alice_bits.push_back(rand()%2);
@@ -90,27 +74,26 @@ void apply_protocol() {
 
 	// An array to tell us which qubits are encoded in which bases
 	std::vector<int> alice_bases;
-	for(int i=0;i<100;i++) {
+	for(int i=0;i<n;i++) {
 		alice_bases.push_back(rand()%2);
 	}
-	std::vector<Circuit*> message;
-	message = encode_message(alice_bits, alice_bases);
-	
-	std::vector<int> bob_bases;
-	for(int i=0;i<100;i++) {
+	std::vector<int> bob_bases,bob_results;
+	for(int i=0;i<n;i++) {
 		bob_bases.push_back(rand()%2);
 	}
-	std::vector<int> bob_results;
-	bob_results = measure_message(message, bob_bases);
-	
-	
+
+	for(int i = 0;i<n;i++){
+		Circuit message = encode_message(alice_bits[i],alice_bases[i]);
+		//std::cout<<"Encoded bit "<<i<<"\n";
+		bob_results.push_back(measure_message(message,bob_bases[i]));
+		//std::cout<<"Bob measured bit "<<i<<'\n';
+	}
 	std::vector<int> alice_key;
 	alice_key = remove_garbage(alice_bases, bob_bases, alice_bits);
-	
 	std::vector<int> bob_key;
 	bob_key = remove_garbage(alice_bases, bob_bases, bob_results);
 	
-	int sample_size = 15;
+	int sample_size = alice_key.size()/2;
 	std::vector<int> bit_selection;
 	
 	for(int i=0;i<sample_size;i++) {
@@ -122,5 +105,15 @@ void apply_protocol() {
 	
 	std::vector<int> alice_sample;
 	alice_sample = sample_bits(alice_key, bit_selection);
-	
+	for(int i = 0;i<sample_size;i++){
+		if(alice_sample[i]!=bob_sample[i]){
+			std::cout<<"Protocol aborted\n";
+			return;
+		}
+	}
+	std::cout<<"Key of length "<<alice_key.size()<<" has been established\n";
+}
+
+int main(){
+	apply_BB84(100);
 }
